4301a58238ad51f38fbdd4f0062ca02a0ec9c7ab
Simon Ding <SimonDing87@gmail.com>
2016-04-07 11:55:03 -0700

    Height-balanced tree checker

---
+++ balancedTree.js
@@ -0,0 +1,58 @@
+var balanced = function(root) {
+  // checks to see if a tree is height-balanced.
+  // distance from all leaf nodes to the root differ no more than 1.
+  var min = Infinity;
+  var max = 0;
+  
+  // use DFS to get to each leaf node
+  
+  var recurse = function(node, depth) {
+    // base case
+    if (!node.children.length) {
+      if (depth < min) {
+        min = depth;
+      }
+      if (depth > max) {
+        max = depth;
+      }
+      return;
+    }
+    
+    // recursive case
+    node.children.forEach(function(child) {
+      recurse(child, depth+1);
+    });
+  }
+  
+  recurse(root, 0);
+  return Math.abs(max - min) <= 1;
+}
+
+var treeNode = function(value) {
+  this.value = value;
+  this.children = [];
+};
+
+treeNode.prototype.addChildren = function(arr) {
+  this.children = this.children.concat(arr);
+}
+
+var node1 = new treeNode(1);
+var node2 = new treeNode(2);
+var node3 = new treeNode(3);
+var node4 = new treeNode(4);
+var node5 = new treeNode(5);
+var node6 = new treeNode(6);
+var node7 = new treeNode(7);
+
+node1.addChildren([node2, node3]);
+node2.addChildren([node4, node5]);
+node3.addChildren([node6, node7]);
+
+// balanced tree
+//      1
+//   2      3
+// 4   5  6   7
+
+//tests
+console.log(balanced(node1) === true);
